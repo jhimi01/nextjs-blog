64288ff198ba1906f2c915fcd1d410979c616714
jhimi01 <talukderjhimi@gmail.com>
2024-04-28 16:59:41 +0600

    first commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 import Navbar from "@/components/navbar/Navbar";
 import Footer from "@/components/footer/Footer";
 
+
 const poppins = Poppins({weight: '400', subsets: ["latin"] });
 
 export const metadata = {
@@ -13,6 +14,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+       <head>
+          <link rel="icon" href="/favicon.ico" />
+          {/* Add additional icon sizes for better compatibility */}
+          <link rel="icon" type="image/png" href="/logo.png" />
+        </head>
       <body className={poppins.className}>
       <Navbar />
         {children}
---
+++ src/components/demoblogs/loading.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function loading() {
+  return (
+    <div>
+      blogs are loading
+    </div>
+  )
+}
